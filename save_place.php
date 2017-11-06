9730d83e4f50f09a1e4e928805780d73b813bc45
Erick Guerrero <erick.g@valles.udg.mx>
2017-11-06 16:50:21 -0600

    Mejora del api
    Se pueden subir archivos en la carpeta /images

--- save_place.php
+++ save_place.php
@@ -10,22 +10,27 @@ include "conex.php";
 
 $nombre = $_POST['nombre'];
 $descripcion = $_POST['descripcion'];
+$foto = 'images/'.basename($_FILES['imagen']['name']);
 
 
-if(guardar($nombre,$descripcion,$conexion)){
+if(guardar($nombre,$descripcion,$foto,$conexion)){
     echo "OK!";
 }else{
     echo "ERROR!";
 }
 
 
-function guardar($nombre, $desc, $conexion){
+function guardar($nombre, $desc,$foto, $conexion){
 
-    $sql = "INSERT INTO lugars(nombre,descripcion) VALUES('$nombre','$desc')";
+    $sql = "INSERT INTO lugars(nombre,descripcion,foto) VALUES('$nombre','$desc','$foto')";
 
     if(mysqli_query($conexion,$sql)){
-        return true;
+        if(move_uploaded_file($_FILES['imagen']['tmp_name'],$foto))
+            return true;
+        else
+            return false;
     }else{
+        echo mysqli_error($conexion);
         return false;
     }
 
